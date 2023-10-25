1ea9183ce4fda4e6111e3fca7467b23415fe5720
Paweł Sidorowicz <psidorowicz@bee2code.com>
2023-10-25 17:03:37 +0200

    after fiddling with nextui and tailwind

--- app/components/ImageCarousel.tsx
+++ app/components/ImageCarousel.tsx
@@ -4,9 +4,9 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const ImageCarousel = ({ images }: { images: Array<string> }) => {
     const renderImages = () => {
-        return images.map((image) =>
+        return images.map((image, index) =>
             <div>
-                <img src={image} alt={image} />
+                <img src={image} alt={image} key={index} />
             </div>
         )
     }
--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,12 +1,15 @@
+import Image from "next/image"
 
 
 const ImageGallery = ({ images }: { images: Array<any> }) => {
     return (
-        <div className="container">
-            <div className="row">
-                {images.map((image) => <div className="col">
-                    <img src={image.image} />
-                </div>)}
+        <div style={{ border: '1px solid gray' }} className='container-fluid '>
+            <div className="row"  >
+                {images.map((image) =>
+                    <div className="col-4" style={{ height: 400, padding: 0, overflow: 'hidden' }}>
+                        <Image src={image.image} alt={image.id} width={800} height={400} />
+                    </div>
+                )}
             </div>
         </div>
     )
